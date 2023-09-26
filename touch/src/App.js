044e0fe2ae3021c3a0d655574a2dba0fa4b0e92c
vicky kumar mishra <mishravicky0141@gmail.com>
2023-09-26 22:09:40 +0530

    loader

--- touch/src/App.js
+++ touch/src/App.js
@@ -1,20 +1,29 @@
 
 import './App.css';
+import { useState,useEffect } from 'react';
 import TouchTyping from './Components/TouchTyping';
 import Join from './Components/LandingPage';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
-
+import LoadingSpinner from './Components/LoadingSpinner';
 import { Route,Routes,BrowserRouter } from 'react-router-dom';
 import Newlesson from './Components/Newlesson';
 import { createContext } from 'react';
 const contextdata=createContext()
-function App() {
 
- 
+
+function App() {
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    // Simulate data fetching or initialization
+    setTimeout(() => {
+      setLoading(false); // Set loading to false when done
+    }, 3000); // Adjust the timeout as needed
+  }, []);
   return (
     <div className="App" >
     
+    {isLoading?(<LoadingSpinner/>):(
   <BrowserRouter>
 <contextdata.Provider >
  
@@ -25,9 +34,11 @@ function App() {
   <Route path='/Join' element={<Join/>}></Route>
   <Route path='/Newlesson' element={<Newlesson/>}></Route>
   <Route path='/Navbar' element={<Navbar/>}></Route>
+  <Route path="/LoadingSpinner" element={<LoadingSpinner/>}></Route>
 </Routes>
 
 </BrowserRouter> 
+)}
     </div>
   );
 }
---
+++ touch/src/Components/LoadingSpinner.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function LoadingSpinner() {
+
+    const myStyle={
+        backgroundColor:'black',
+        height:'100vh',
+        // marginTop:'-70px',
+       
+        fontSize:'50px',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center center', // This centers the image both horizontally and vertically
+        
+    };
+
+  return (
+    <div style={myStyle}>
+      
+      <div id="spinner" className="spinner-grow text-warning" style={{width: '4rem', height: '5rem', role:"status",borderRadius:'50%' ,color:'white',marginTop:"10rem"}}>
+  <span className="visually-hidden" style={{color:'white'}}>Loading...</span>
+</div>
+
+    </div>
+  )
+}
